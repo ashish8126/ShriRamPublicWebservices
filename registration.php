e913f66ed003b8430e9ab808cb6010171a27720b
ashish8126 <ashish8126@gmail.com>
2016-12-09 21:39:40 +0530

    Ashish

---
+++ registration.php
@@ -0,0 +1,26 @@
+<?php    
+     include 'database.php';
+/*
+ * @author Ashish Viltoriya             08/12/2016
+ * 
+ */
+  
+
+    $name  = $_POST['name'];
+    $email = $_POST['email'];
+    $phone = $_POST['phone'];
+    $password = $_POST['password'];
+
+    
+   
+    $name = "Ashish";
+    $email = "ashihsh@gmail.com";
+    $phone = 787878787;
+    $password = "sdfds";
+    
+    $sql = "insert into register_user (name, phone, email,password) values ('$name', $phone, '$email','$password')";
+    
+    $result = mysqli_query($conn,$sql);
+    echo 'Success';
+    
+?>
